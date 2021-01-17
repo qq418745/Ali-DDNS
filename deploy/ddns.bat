995d6b1ecab0699569dff25a1275f064086c323b
考拉 <work@liuchengming.cn>
2021-01-17 17:08:52 +0800

    同步bat

--- deploy/ddns.bat
+++ deploy/ddns.bat
@@ -1,4 +1,4 @@
 @echo off
 %1 mshta vbscript:CreateObject("WScript.Shell").Run("%~s0 ::",0,FALSE)(window.close)&&exit
-java -jar .\hjcDDNS.jar >.\ddns-log-%date% 2>&1 &
+java -jar  .\ddns.jar  > .\ddns.log 2>&1 &
 exit
